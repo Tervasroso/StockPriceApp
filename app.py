797e2f3a9af3b0c6fac64f064e0f4c5b7def46e7
Tervasroso <tuokivilahti@gmail.com>
2023-08-23 12:10:51 +0300

    updated comments

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ st.write("""
          # Nokian vuoden 2022 osakekurssi ja volyymi
          """)
 
-# Määritä ticker symboli (Nokian symboli on NOK)
-# A ticker is a type of stock symbol that describes information about the stock of a company.
+# Määritä ticker symboli (Nokian symboli on NOK) - Define ticker Symbol
+# Ticker symboli on uniikki koodi, jolla tunnistetaan pörssiyhtiö. # A ticker is a type of stock symbol that describes information about the stock of a company.
+# All stock symbals https://stockanalysis.com/stocks/
 
 tickerSymbol = 'NOK'
-# Haetaan tämän symbolin data.
+# Haetaan tämän symbolin data. - get data for this symbol
 tickerData = yf.Ticker(tickerSymbol)
-# Haetaan tämän tickerin historiatietoja
+# Haetaan tämän tickerin historiatietoja - get historic data for this Ticker
 tickerDf = tickerData.history(period='1d', start='2022-1-1', end='2022-12-31')
 
 st.write("""
